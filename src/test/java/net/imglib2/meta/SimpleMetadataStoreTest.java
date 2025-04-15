7c3ebb2a73691e7675b56e3b6794c54c674d805e
Curtis Rueden <ctrueden@wisc.edu>
2025-04-15 16:57:34 -0500

    Change unit test to assert rather than print

--- src/test/java/net/imglib2/meta/SimpleMetadataStoreTest.java
+++ src/test/java/net/imglib2/meta/SimpleMetadataStoreTest.java
@@ -2,7 +2,9 @@ package net.imglib2.meta;
 
 import net.imagej.axis.Axes;
 import net.imagej.axis.AxisType;
+import net.imagej.axis.CalibratedAxis;
 import net.imagej.axis.DefaultLinearAxis;
+import net.imagej.axis.TypedAxis;
 import net.imglib2.Localizable;
 import net.imglib2.RealLocalizable;
 import net.imglib2.RealRandomAccessible;
@@ -24,6 +26,8 @@ import java.util.Arrays;
 import java.util.function.BiConsumer;
 import java.util.function.Supplier;
 
+import static org.junit.Assert.*;
+
 /** Tests {@link SimpleMetadataStore} functionality. */
 public class SimpleMetadataStoreTest {
 
@@ -39,10 +43,10 @@ public class SimpleMetadataStoreTest {
             t.set(Long.parseLong(sb.toString()));
         };
         FunctionRandomAccessible<DoubleType> image = new FunctionRandomAccessible<>(5, f, s);
-        System.out.println(image.getAt(1,2,3,4,5));
+        assertEquals(12345.0, image.getAt(1,2,3,4,5).get(), 0.0);
         // rotate
         MixedTransformView<DoubleType> v = Views.rotate(image, 3, 2);
-        System.out.println(v.getAt(1,2,3,4,5));
+        assertEquals(12435.0, v.getAt(1,2,3,4,5).get(), 0.0);
         // translate
 //        v = Views.permute(image, 0, 1);
 //        System.out.println(v.getAt(1, 2, 3, 4, 5));
@@ -64,38 +68,40 @@ public class SimpleMetadataStoreTest {
 
         // Query metadata type-unsafely (using key strings).
         Object authorObject = store.get("author").get().get();
-        System.out.println(authorObject);
+        assertEquals("foo Selzer", authorObject);
 
         // Query metadata type-safely (but still using a key string).
         String authorString = store.get("author", String.class).get().get();
-        System.out.println(authorString);
+        assertEquals("foo Selzer", authorString);
 
         // An actually nice window into groups of metadata.
         Attribution attribution = Metadata.attribution(store);
         String author = attribution.author();
         String citation = attribution.citation();
-        System.out.println(author);
-        System.out.println(citation);
+        assertEquals("foo Selzer", author);
+        assertNull(citation);
 
         AxisType axis0Type = calibration.axis(0).type();
         AxisType axis2Type = calibration.axis(2).type();
-        System.out.println("Axis 0 type: " + axis0Type);
-        System.out.println("Axis 2 type: " + axis2Type);
+        assertSame(Axes.X, axis0Type);
+        assertSame(Axes.Z, axis2Type);
 
-        System.out.println(store.get("axis", 0).get().get());
-        System.out.println(store.get("axis", 2).get().get());
+        CalibratedAxis axis0 = store.get("axis", 0, CalibratedAxis.class).get().get();
+        CalibratedAxis axis2 = store.get("axis", 2, CalibratedAxis.class).get().get();
+        assertSame(Axes.X, axis0.type());
+        assertSame(Axes.Z, axis2.type());
 
         MetadataItem<?> lutItem = store.get("lut", 3).get();
-        System.out.println(lutItem.getAt(0, 0, 0, 0, 0));
-        System.out.println(lutItem.getAt(0, 0, 0, 1, 0));
-        System.out.println(lutItem.getAt(0, 0, 0, 2, 0));
+        assertEquals("red", lutItem.getAt(0, 0, 0, 0, 0));
+        assertEquals("green", lutItem.getAt(0, 0, 0, 1, 0));
+        assertEquals("blue", lutItem.getAt(0, 0, 0, 2, 0));
 
         // Test viewing metadata based on a View of the data
         MetadataStore storeView = Metadata.view(store, v);
         lutItem = storeView.get("lut", 2).get();
-        System.out.println(lutItem.getAt(0, 0, 0, 0, 0));
-        System.out.println(lutItem.getAt(0, 0, 1, 0, 0));
-        System.out.println(lutItem.getAt(0, 0, 2, 0, 0));
+        assertEquals("red", lutItem.getAt(0, 0, 0, 0, 0));
+        assertEquals("green", lutItem.getAt(0, 0, 1, 0, 0));
+        assertEquals("blue", lutItem.getAt(0, 0, 2, 0, 0));
     }
 
     @Test
@@ -112,66 +118,30 @@ public class SimpleMetadataStoreTest {
             t.set(Long.parseLong(digits));
         };
         FunctionRealRandomAccessible<DoubleType> image = new FunctionRealRandomAccessible<>(5, f, s);
-        System.out.println(image.getAt(1,2,3,4,5));
+        assertEquals(1.02030405E9, image.getAt(1,2,3,4,5).get(), 0.0);
         // rotate
         double[] scales = {1.1, 2.2, 3.3, 4.4, 5.5};
         double[] translations = {9.9, 8.8, 7.7, 6.6, 5.5};
         AffineGet transform = new ScaleAndTranslation(scales, translations);
         AffineRealRandomAccessible<DoubleType, AffineGet> v = RealViews.affineReal(image, transform);
-        System.out.println(v.getAt(1,2,3,4,5));
-        // translate
-//        v = Views.permute(image, 0, 1);
-//        System.out.println(v.getAt(1, 2, 3, 4, 5));
+        assertEquals(8.0909090909090918E17, v.getAt(1,2,3,4,5).get(), 0.0);
 
         // Populate some metadata.
 
         MetadataStore store = new SimpleMetadataStore(5);
-        store.add("author", "foo Selzer");
-
-        Calibration calibration = Metadata.calibration(store);
-        calibration.setAxis(new DefaultLinearAxis(Axes.X), 0);
-        calibration.setAxis(new DefaultLinearAxis(Axes.Y), 1);
-        calibration.setAxis(new DefaultLinearAxis(Axes.Z), 2);
-        calibration.setAxis(new DefaultLinearAxis(Axes.CHANNEL), 3);
-        calibration.setAxis(new DefaultLinearAxis(Axes.TIME), 4);
 
         RealRandomAccessible<StringBuilder> coordStrings = makeCoordStrings();
         store.add("coords", coordStrings, 0, 1, 2);
 
-        // Query metadata type-unsafely (using key strings).
-        Object authorObject = store.get("author").get().get();
-        System.out.println(authorObject);
-
-        // Query metadata type-safely (but still using a key string).
-        String authorString = store.get("author", String.class).get().get();
-        System.out.println(authorString);
-
-        // An actually nice window into groups of metadata.
-        Attribution attribution = Metadata.attribution(store);
-        String author = attribution.author();
-        String citation = attribution.citation();
-        System.out.println(author);
-        System.out.println(citation);
-
-        AxisType axis0Type = calibration.axis(0).type();
-        AxisType axis2Type = calibration.axis(2).type();
-        System.out.println("Axis 0 type: " + axis0Type);
-        System.out.println("Axis 2 type: " + axis2Type);
-
-        System.out.println(store.get("axis", 0).get().get());
-        System.out.println(store.get("axis", 2).get().get());
-
         MetadataItem<?> coordsItem = store.get("coords", 0).get();
-        System.out.println(coordsItem.getAt(0.3, 0.6, 0.9, 1.2, 1.5));
-        System.out.println(coordsItem.getAt(0, 0, 0, 1, 0));
-        System.out.println(coordsItem.getAt(0, 0, 0, 2, 0));
+        assertEquals("(0.3, 0.6, 0.9)", coordsItem.getAt(0.3, 0.6, 0.9, 1.2, 1.5).toString());
 
         // Test viewing metadata based on a View of the data
         MetadataStore storeRealView = Metadata.view(store, v);
         coordsItem = storeRealView.get("coords", 2).get();
-        System.out.println(coordsItem.getAt(1, 2, 3, 4, 5));
-        System.out.println(coordsItem.getAt(5, 6, 7, 8, 9));
-        System.out.println(coordsItem.getAt(-10, -11, -12, -13, -14));
+        assertEquals("(-8.090909090909092, -3.090909090909091, -1.4242424242424243)", coordsItem.getAt(1, 2, 3, 4, 5).toString());
+        assertEquals("(-4.454545454545455, -1.272727272727273, -0.21212121212121238)", coordsItem.getAt(5, 6, 7, 8, 9).toString());
+        assertEquals("(-18.09090909090909, -9.0, -5.969696969696971)", coordsItem.getAt(-10, -11, -12, -13, -14).toString());
     }
 
     private RealRandomAccessible<StringBuilder> makeCoordStrings() {
