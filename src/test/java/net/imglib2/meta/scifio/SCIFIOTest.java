b14a2faed7686b5a4f3d9282d0805524653333e8
Gabriel Selzer <gjselzer@wisc.edu>
2025-11-01 17:32:30 -0500

    SCIFIOTest: Generate our own image

--- src/test/java/net/imglib2/meta/scifio/SCIFIOTest.java
+++ src/test/java/net/imglib2/meta/scifio/SCIFIOTest.java
@@ -1,49 +1,42 @@
 package net.imglib2.meta.scifio;
 
-import io.scif.img.ImgUtilityService;
+import io.scif.DefaultImageMetadata;
+import io.scif.DefaultMetadata;
 import io.scif.img.SCIFIOImgPlus;
-import io.scif.img.converters.PlaneConverterService;
-import io.scif.services.DatasetIOService;
-import io.scif.services.InitializeService;
-import net.imagej.Dataset;
+import net.imagej.axis.Axes;
+import net.imagej.axis.DefaultLinearAxis;
+import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.meta.Metadata;
 import net.imglib2.meta.MetadataStore;
 import net.imglib2.meta.calibration.Axis;
 import net.imglib2.meta.calibration.Calibration;
 import net.imglib2.meta.general.General;
+import net.imglib2.type.numeric.real.DoubleType;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.scijava.Context;
-import org.scijava.app.StatusService;
-import org.scijava.io.handle.DataHandleService;
-import org.scijava.io.location.FileLocation;
-
-import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Tests the ability to wrap a {@link SCIFIOImgPlus} into a {@link MetadataStore}.
- * <p>
- * Note that this test currently requires you download <a href="https://samples.scif.io/mitosis-ics.zip">this image</a>
- * to the repository root.
- * </p>
  *
  * @author Gabriel Selzer
  */
 public class SCIFIOTest {
 
-    static SCIFIOImgPlus<?> img;
+    static SCIFIOImgPlus<DoubleType> img;
     static MetadataStore store;
 
     @BeforeClass
-    public static void setUp() throws IOException {
-        // Note that this test currently requires you download https://samples.scif.io/mitosis-ics.zip
-        // to the repository root.
-        Context ctx = new Context();
-        DatasetIOService dio = ctx.getService(DatasetIOService.class);
-        Dataset data = dio.open(new FileLocation("tubhiswt-4D/tubhiswt_C0_TP0.ome.tif"));
-        img = (SCIFIOImgPlus<?>) data.getImgPlus();
+    public static void setUp() {
+        img = new SCIFIOImgPlus<>(ArrayImgs.doubles(5, 5));
+        img.setMetadata(new DefaultMetadata());
+        img.setImageMetadata(new DefaultImageMetadata());
+        img.getImageMetadata().setAxes( //
+            new DefaultLinearAxis(Axes.X), //
+            new DefaultLinearAxis(Axes.Y) //
+        );
+        img.getImageMetadata().setName("Test Image");
         store = new SCIFIOMetadataStore(img);
     }
 
